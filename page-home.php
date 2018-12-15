02f95d6fbae0a21bb3ceb8f53a3c2e6d6a6674cd
93lucasp <spezzanoluca@gmail.com>
2018-12-15 12:02:47 +0100

    working

--- page-home.php
+++ page-home.php
@@ -2,24 +2,27 @@
 <?php /* Template Name: home */ ?>
 <?php get_header(); ?>
 <body class="home d-flex align-items-center justify-content-center position-relative">
-<video autoplay muted loop class="video-bg">
+    <video autoplay muted loop class="video-bg">
         <source src="<?php bloginfo(template_directory); ?>/images/galaxy.mp4" type="video/mp4">
         Your browser does not support HTML5 video.
     </video>
+    <div class="pattern-home" style="background-image: url('<?php bloginfo(template_directory); ?>/images/pattern.png')"></div>
     <img src="<?php bloginfo(template_directory); ?>/images/lem-white.svg" alt="" class="logo logo--absolute">
-    <div class="container">
+    <div class="container mt-4">
         <div class="row">
-            <div class="col-lg-6">
+            <div class="col-6 col-lg-5 ml-auto">
                 <a href="/index.php/projects/" class="cta d-flex align-items-center justify-content-center">
-                    <div class="text-center">
+                <div class="cta__line"></div>
+                    <div class="text-center d-flex align-items-center">
                         <h1>Projects</h1>
                         <i class="fas fa-arrow-right"></i>
                     </div>
                 </a>
             </div>
-            <div class="col-lg-6">
+            <div class="col-6 col-lg-5 mr-auto">
                 <a href="#" class="cta d-flex align-items-center justify-content-center">
-                    <div class="text-center">
+                    <div class="cta__line"></div>
+                    <div class="text-center d-flex align-items-center">
                         <h1>Studio</h1>
                         <i class="fas fa-arrow-right"></i>
                     </div>
@@ -27,5 +30,18 @@
             </div>
         </div>
 
+    </div>
+    <div class="footer-home py-4 mt-4">
+        <div class="container d-flex align-items-center justify-content-between">
+            <div class="d-flex align-items-center">
+                <p>↑ 45°04'13" → 7°41'12"</p>
+                <div class="footer__line"></div>
+                
+            </div>
+            <div class="d-flex align-items-center">
+                <a href="#">info@agenzialem.it</a>
+                <div class="footer__line "></div>
+            </div>
+        </div>
     </div>
 	
--- page-projects.php
+++ page-projects.php
@@ -1,6 +1,23 @@
 <?php /* Template Name: projects */ ?>
 <?php get_header(); ?>
 <body class="projects-page">
+<div class="navbar">
+    <div class="container d-flex align-items-center justify-contente-between">
+        <a href="/">
+        <img src="<?php bloginfo(template_directory); ?>/images/lem-dark.svg" alt="">
+        </a>
+        <div>
+            <ul class="list-unstyled d-flex">
+                <li>
+                    <a href="/index.php/projects/" class="link-projects">Projects</a>
+                </li>
+                <li class="mx-3">
+                    <a href="/studio.html">Studio</a>
+                </li>
+            </ul>
+        </div>
+    </div>
+</div>
     <div class="container-fluid">
         <nav class="my-5">
             <div class="nav nav-tabs justify-content-center" id="nav-tab" role="tablist">
@@ -11,7 +28,7 @@
             </div>
         </nav>
         <div class="tab-content" id="nav-tabContent">
-        <div class="tab-pane fade" id="nav-allProjects" role="tabpanel" aria-labelledby="nav-allProjects-tab">
+            <div class="tab-pane fade" id="nav-allProjects" role="tabpanel" aria-labelledby="nav-allProjects-tab">
                 <div class="container ">
                     <div class="row">
                         <div class="col-lg-4 border-bottom">
@@ -32,70 +49,30 @@
                     </div>
                 </div>
                 <div class="container">
-                    <div class="row <?php the_field('link'); ?>">
-                        <div class="col-lg-4 border-bottom">
-                        <a href="<?php the_field('url'); ?>">
-                        <h6 class="font-weight-bold" ><?php the_field('title'); ?></h6>
-                        </a>
-                            
-                        </div>
-                        <div class="col-lg-2 border-bottom" >
-                            <h6 class="font-weight-bold"><?php the_field('city'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom" >
-                            <h6 class="font-weight-bold"><?php the_field('country'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom">
-                            <h6 class="font-weight-bold"><?php the_field('typology'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom">
-                            <h6 class="font-weight-bold"><?php the_field('year'); ?></h6>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-4 border-bottom">
-                            <h6 class="font-weight-bold" ><?php the_field('title2'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom" >
-                            <h6 class="font-weight-bold"><?php the_field('city2'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom" >
-                            <h6 class="font-weight-bold"><?php the_field('country2'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom">
-                            <h6 class="font-weight-bold"><?php the_field('typology2'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom">
-                            <h6 class="font-weight-bold"><?php the_field('year2'); ?></h6>
-                        </div>
-                    </div>
-                    <div class="row">
-                        <div class="col-lg-4 border-bottom">
-                            <h6 class="font-weight-bold" ><?php the_field('title3'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom" >
-                            <h6 class="font-weight-bold"><?php the_field('city3'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom" >
-                            <h6 class="font-weight-bold"><?php the_field('country3'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom">
-                            <h6 class="font-weight-bold"><?php the_field('typology3'); ?></h6>
-                        </div>
-                        <div class="col-lg-2 border-bottom">
-                            <h6 class="font-weight-bold"><?php the_field('year3'); ?></h6>
-                        </div>
-                    </div>
-                   
                 
-
-                   
-                    <?php while ( have_rows('test') ) : the_row();?>
-
-                       
+                    <?php while ( have_rows('row_projects') ) : the_row();?>
+                    <a href="<?php the_sub_field('url'); ?>" class="table--listProjects <?php the_sub_field('haslink'); ?>">
+                        <div class="row">
+                            <div class="col-4 border-bottom d-flex">
+                                <img src="<?php bloginfo(template_directory); ?>/images/icons/link.svg" alt="" class="mr-2 icon">
+                                <h6 class="mb-0 py-3"><?php the_sub_field('title'); ?></h6>
+                            </div>
+                            <div class="col-2 border-bottom">
+                                <h6 class="mb-0 py-3"><?php the_sub_field('city'); ?></h6>
+                            </div>
+                            <div class="col-2 border-bottom">
+                                <h6 class="mb-0 py-3"><?php the_sub_field('country'); ?></h6>
+                            </div>
+                            <div class="col-2 border-bottom">
+                                <h6 class="mb-0 py-3"><?php the_sub_field('tipology'); ?></h6>
+                            </div>
+                            <div class="col-2 border-bottom">
+                                <h6 class="mb-0 py-3"><?php the_sub_field('year'); ?></h6>
+                            </div>
+                        </div>
+                    </a>
                         
-                        <h1><?php the_sub_field('title'); ?></h1>
-
+                    
                     <?php endwhile;?>
                 </div>
             </div>
@@ -103,19 +80,18 @@
                 <div class="row">
                     <?php query_posts('post_type=selected_projects'); ?>
                     <?php while (have_posts()) : the_post(); ?>
-                        <div class="col-lg-4">
+                        <div class="col-6 col-lg-4">
                             <div class="project" style=" background-image: url(<?php the_field('imgmain'); ?>)">
                                 <div class="project__opacity"></div>
                                 <div class="project__info">
                                     <a href="<?php the_permalink() ?>" class="project__title"><?php the_title(); ?></a>
-                                    <p class="project__location"><?php the_field('location'); ?> <?php the_field('selected'); ?></p>
+                                    <p class="project__location"><?php the_field('location'); ?></p>
                                 </div>
                             </div>
                         </div>
-	                <?php endwhile;?>
+                    <?php endwhile;?>
                 </div>
             </div>
-            
         </div>
 
     </div>
